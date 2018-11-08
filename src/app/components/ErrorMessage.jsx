863ef40a787d323ac729f641dc4fa53a1cb52126
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2018-11-08 21:20:05 +0200

    Add comments

--- src/app/components/ErrorMessage.jsx
+++ src/app/components/ErrorMessage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { Message, List } from 'semantic-ui-react';
 
+/**
+ * An error to show if something bad has pappened while fetching rates
+ */
 const ErrorMessage = ({ reason }) => (
     <Message error>
         <p>Failed to load list of rates</p>
--- src/app/components/FetchButton.jsx
+++ src/app/components/FetchButton.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 
+/**
+ * The button triggers rates fetching
+ */
 const FetchButton = ({ loading, onClick }) => (
     <Button onClick={onClick} disabled={loading} size="big" primary fluid>
         Fetch Rates
