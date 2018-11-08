a2d5a638a2b98db123f711df155d0452d81a2086
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2018-11-08 18:59:16 +0200

    Add top and bottom margin

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Container } from 'semantic-ui-react';
 
 import { ReduxProvider } from '../../providers';
 import RatesList from '../containers/RatesList';
 import FetchButton from '../containers/FetchButton';
 
+const StyledContainer = styled(Container)`
+    margin-top: 20px;
+    margin-bottom: 20px;
+`;
+
 const App = () => (
     <ReduxProvider>
-        <Container>
+        <StyledContainer>
             <FetchButton />
             <RatesList />
-        </Container>
+        </StyledContainer>
     </ReduxProvider>
 );
 
